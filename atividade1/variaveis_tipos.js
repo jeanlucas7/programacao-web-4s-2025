244a3de2b5d497316513b60eac9c128da067fad0
jeanlucas7 <acesso.jeanlucas@gmail.com>
2025-05-22 21:30:06 -0300

    atividade5

---
+++ atividade1/variaveis_tipos.js
@@ -0,0 +1,16 @@
+const n = 3;
+
+var j = 6;
+var j = 7;
+
+let nome = 'Jean';
+let numero = "1245";
+
+console.log("Meu nome e: " + nome + " Telefone: " + numero);
+
+let x = 1;
+x = 0.5;
+x = 3.14;
+
+let b = true;
+b = false;
